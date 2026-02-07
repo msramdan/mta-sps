e40223afcf877897b8123d305a08af97979d7e6e
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2026-02-07 10:49:11 +0700

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,5 @@ class UserSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(length: 10),
         ]);
-
-        User::factory()->count(10)->create();
     }
 }
