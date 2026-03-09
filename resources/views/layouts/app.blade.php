0fcade41dc551361f0847626b4386a9e6f6134be
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2026-03-09 12:12:31 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-9 col-12">
-                                <p class="footer-text f-w-600 mb-0">© {{ date('Y') }} {{ config('app.name') }}.</p>
+                                <p class="footer-text f-w-600 mb-0">© {{ date('Y') }} Marketrax.</p>
                             </div>
                         </div>
                     </div>
